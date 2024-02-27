0e9a741a2106734a71e208c2e6aed38eca0551a7
Doggydo123 <stufkensd@gmail.com>
2024-02-27 14:12:34 +1300

    Refinements

---
+++ Services/HistoricalData.py
@@ -0,0 +1,8 @@
+from Models.HistoricalData import HistoricalData
+from ..DB import DBAutomation as DB
+
+def getAllHistoricalDataForStock(stockCipher: str):
+    print()
+
+def getPeriodOfHistoricalDataForStock(stockCipher, startDate, endDate):
+    print()
---
+++ Services/StockService.py
@@ -0,0 +1,16 @@
+import string
+from typing import List
+from Models.Stock import Stock
+from ..DB import DBAutomation as DB
+
+def getStock(cipher: str):
+    print("todo")
+
+def getStocks(ciphers: List[str]):
+    print("Get all stocks in list")
+
+def insertStock(stock: Stock):
+    print("")
+
+def insertStocks(stocks: List[Stock]):
+    print()
